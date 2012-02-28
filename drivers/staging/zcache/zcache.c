25f425ed42e39183f38c5868bb9ca3fa09a9fa35
Seth Jennings <sjenning@linux.vnet.ibm.com>
2012-02-28 16:01:37 -0600

    staging: zcache: fix length type mismatch
    This fixes a type mismatch in the compression code where
    a size_t pointer was cast to a unsigned int pointer.  On
    little endian archs, there is no issue.  However on big
    endian archs, the value is incorrect, taking the high
    order bits and truncating the lower order bits.
    
    Signed-off-by: Seth Jennings <sjenning@linux.vnet.ibm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/zcache/zcache.c
+++ drivers/staging/zcache/zcache.c
@@ -937,13 +937,13 @@ static atomic_t zcache_curr_pers_pampd_count = ATOMIC_INIT(0);
 static unsigned long zcache_curr_pers_pampd_count_max;
 
 /* forward reference */
-static int zcache_compress(struct page *from, void **out_va, size_t *out_len);
+static int zcache_compress(struct page *from, void **out_va, unsigned *out_len);
 
 static void *zcache_pampd_create(struct tmem_pool *pool, struct tmem_oid *oid,
 				 uint32_t index, struct page *page)
 {
 	void *pampd = NULL, *cdata;
-	size_t clen;
+	unsigned clen;
 	int ret;
 	bool ephemeral = is_ephemeral(pool);
 	unsigned long count;
@@ -1038,7 +1038,7 @@ static struct tmem_pamops zcache_pamops = {
 static DEFINE_PER_CPU(unsigned char *, zcache_dstmem);
 #define ZCACHE_DSTMEM_ORDER 1
 
-static int zcache_compress(struct page *from, void **out_va, size_t *out_len)
+static int zcache_compress(struct page *from, void **out_va, unsigned *out_len)
 {
 	int ret = 0;
 	unsigned char *dmem = __get_cpu_var(zcache_dstmem);
@@ -1051,7 +1051,7 @@ static int zcache_compress(struct page *from, void **out_va, size_t *out_len)
 	from_va = kmap_atomic(from, KM_USER0);
 	mb();
 	rret = zcache_comp_op(ZCACHE_COMPOP_COMPRESS, from_va, PAGE_SIZE, dmem,
-        (unsigned int *)out_len);
+        out_len);
     BUG_ON(ret);
 	*out_va = dmem;
 	kunmap_atomic(from_va, KM_USER0);
