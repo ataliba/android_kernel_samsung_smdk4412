3e583b134598bc1db8d9adfc2d0f61c7d29dedc7
Gustavo <gustavo.santos0106@gmail.com>
2014-01-14 23:50:05 -0200

    build: fix build.sh

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ cd /home/gustavo/zip_tmp
 rm *.zip
 zip -r cm-11-kernel-$CURRENTDATE-CWM.zip ./
 cp ./*.zip /home/gustavo/zips/cm-11-kernel-$CURRENTDATE-CWM.zip
+cd /home/gustavo/kernel
 make -j3 zImage CONFIG_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/initramfs/cm_initramfs.twrp.list"
 cp /home/gustavo/kernel/arch/arm/boot/zImage /home/gustavo/zip_tmp/
 CURRENTDATE=$(date +"%d-%m")
@@ -18,6 +19,7 @@ cd /home/gustavo/zip_tmp
 rm *.zip
 zip -r cm-11-kernel-$CURRENTDATE-TWRP.zip ./
 cp ./*.zip /home/gustavo/zips/cm-11-kernel-$CURRENTDATE-TWRP.zip
+cd /home/gustavo/kernel
 make -j3 zImage CONFIG_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/initramfs/omni_initramfs.list"
 cp /home/gustavo/kernel/arch/arm/boot/zImage /home/gustavo/zip_tmp/
 CURRENTDATE=$(date +"%d-%m")
@@ -25,6 +27,7 @@ cd /home/gustavo/zip_tmp
 rm *.zip
 zip -r omni-4.4-kernel-$CURRENTDATE-CWM.zip ./
 cp ./*.zip /home/gustavo/zips/omni-4.4-kernel-$CURRENTDATE-CWM.zip
+cd /home/gustavo/kernel
 make -j3 zImage CONFIG_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/initramfs/omni_initramfs.twrp.list"
 cp /home/gustavo/kernel/arch/arm/boot/zImage /home/gustavo/zip_tmp/
 CURRENTDATE=$(date +"%d-%m")
@@ -32,6 +35,7 @@ cd /home/gustavo/zip_tmp
 rm *.zip
 zip -r omni-4.4-kernel-$CURRENTDATE-TWRP.zip ./
 cp ./*.zip /home/gustavo/zips/omni-4.4-kernel-$CURRENTDATE-TWRP.zip
+cd /home/gustavo/kernel
 make -j3 zImage CONFIG_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/initramfs/slimkat_initramfs.list"
 cp /home/gustavo/kernel/arch/arm/boot/zImage /home/gustavo/zip_tmp/
 CURRENTDATE=$(date +"%d-%m")
@@ -39,6 +43,7 @@ cd /home/gustavo/zip_tmp
 rm *.zip
 zip -r slimkat-kernel-$CURRENTDATE-CWM.zip ./
 cp ./*.zip /home/gustavo/zips/slimkat-kernel-$CURRENTDATE-CWM.zip
+cd /home/gustavo/kernel
 make -j3 zImage CONFIG_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/initramfs/slimkat_initramfs.twrp.list"
 cp /home/gustavo/kernel/arch/arm/boot/zImage /home/gustavo/zip_tmp/
 CURRENTDATE=$(date +"%d-%m")
