bc75188365578e2f7d834655f0cdf55bc3eff923
Gustavo <gustavo.santos0106@gmail.com>
2014-01-14 23:26:51 -0200

    build: fix build

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 rm /home/gustavo/zips/*.zip
-/home/gustavo/kernel/usr/build_scripts/build_kernel.sh
+cp /home/gustavo/kernel/arch/arm/configs/kernel_defconfig /home/gustavo/kernel/.config
+make -j3
 find -name '*.ko' -exec cp -av {} /home/gustavo/kernel/usr/galaxys2_initramfs_files/modules/ \;
 chmod 644 /home/gustavo/kernel/usr/galaxys2_initramfs_files/modules/*
 /home/gustavo/toolchain/bin/arm-linux-androideabi-strip --strip-unneeded /home/gustavo/kernel/usr/galaxys2_initramfs_files/modules/*
